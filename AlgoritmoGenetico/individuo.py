6d330539a8b71aa12eb58920ce093d64f74cea6e
Gabriel Afonso Pantoja <44549112+kriterius2@users.noreply.github.com>
2020-12-04 19:47:29 -0300

    Update individuo.py

--- AlgoritmoGenetico/individuo.py
+++ AlgoritmoGenetico/individuo.py
@@ -17,10 +17,11 @@ class Individuo(object):
     for rainha in self.tabuleiro.rainhas:
       linha = bin(rainha.linha)[2:]
       self.cromossomos.append(linha)
-  
-  def calculaFitness(self):
+ 
+#como no slide da aula dizia que o fitness deve retornar valores melhores para individuos melhores, decidimos transformar o problema em maximização 
+  def calculaFitness(self): 
     self.tabuleiro.calculaCusto()
-    custoMax = int(math.factorial(8)/(2*math.factorial(8-2)))
-    custo = self.tabuleiro.custo
-    fitness = custoMax - custo
+    custoMax = int(math.factorial(8)/(2*math.factorial(8-2))) #calculo do custo maximo de ataques possivel
+    custo = self.tabuleiro.custo 
+    fitness = custoMax - custo #quanto maior o fitness, menos ataques entre pares de rainhas
     self.fitness = fitness
