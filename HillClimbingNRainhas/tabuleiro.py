d5820858c3bab1d497066a973a93970c721dbfdf
Rafael da Silva Lisboa <48603408+RaffeuL@users.noreply.github.com>
2020-12-04 19:36:29 -0300

    Update tabuleiro.py

--- HillClimbingNRainhas/tabuleiro.py
+++ HillClimbingNRainhas/tabuleiro.py
@@ -11,43 +11,33 @@ class Tabuleiro(object):
     self.rainhas = []
     self.estados = []
     self.iteracoes = 0
-    self.maxInt = 0
+    self.maxInt = 0 #Para a condição de parada das tentativas de mudar o tabuleiro
   
   def criaTabuleiro(self): #Uma rainha em cada coluna mas a linha é aleatoria
     for x in range(self.numRainhas):
       linha = random.randint(0,self.numRainhas-1)
-      rainha = Rainha(linha,x)
+      rainha = Rainha(linha,x) #Cria uma rainha com linha aleatória em cada coluna
       self.rainhas.append(rainha)
     for rainha in self.rainhas: #Atualiza o custo de todas as rainhas logo depois de criar o tabuleiro
       self.verificaRainha(rainha)
     self.calculaCusto()
     self.estados.append(self.custo)
-    print("CUSTO INICIAL: ",self.custo)
+    print("CUSTO INICIAL: ",self.custo) #Calcula o custo do tabuleiro inicial
     self.montaMatriz()
-
-  def criaTabuleiroAleatorio(self): #Cria um tabuleiro full aleatorio
-    for x in range(self.numRainhas):
-      linha = random.randint(0,self.numRainhas-1)
-      coluna = random.randint(0,self.numRainhas-1)
-      rainha = Rainha(linha,coluna)
-      if rainha in self.rainhas:
-        continue
-      self.rainhas.append(rainha)
-    self.montaMatriz()
-  
+    
   def executa(self): #Faz a movimentação de cada rainha atualizando o custo total do tabuleiro
     for rainha in self.rainhas:
       self.HillClimbing(rainha)
       self.calculaCusto()
       self.estados.append(self.custo)#Adiciona o novo custo na lista de estados
       self.maxInt += 1
-    if self.custo == 0 or self.maxInt >= 100:
+    if self.custo == 0 or self.maxInt >= 100: #Enquanto não alcançar o minimo global ou o máximo de execuções, ele continua tendando mudar o tabuleiro
       return self.custo #Retorna o custo atual
     else:
       self.executa()
 
   def verificaRainha(self,Rainha):
-    risco = 0
+    risco = 0 #Verifica o risco de cada rainha calculando se há alguma rainha na mesma linha, coluna, ou diagonais, tanto principal quanto secundária
     for vizinha in self.rainhas:
       if vizinha != Rainha:
         if vizinha.linha == Rainha.linha:
@@ -65,12 +55,12 @@ class Tabuleiro(object):
       self.iteracoes += 1
       linhaCandidata = random.randint(0,self.numRainhas-1)
       candidata = Rainha(linhaCandidata,rainha.coluna)
-      self.verificaRainha(candidata)
-      if candidata.risco > rainha.risco or rainha.risco == 0: #Se a candidata tiver um custo maior que a atual, ele para o laço, portanto para a função também
+      self.verificaRainha(candidata) 
+      if candidata.risco > rainha.risco or rainha.risco == 0: #Se a candidata tiver um risco maior que a atual, ele para o laço, portanto parando a função também
         break
       rainha.linha = candidata.linha
-      rainha.coluna = candidata.coluna
-      self.verificaRainha(rainha)#Faz a troca de posição e recalcula o novo custo
+      rainha.coluna = candidata.coluna #Substitui a rainha no tabuleiro pela nova rainha candidata melhor
+      self.verificaRainha(rainha)#Recalcula o novo custo total
 
   def montaMatriz(self): #Uma função para printar uma matriz representativa do tabuleiro
     matrizR = np.zeros([self.numRainhas,self.numRainhas],dtype = int)
@@ -81,6 +71,6 @@ class Tabuleiro(object):
   def calculaCusto(self):
     custo = 0
     for i in self.rainhas:
-      self.verificaRainha(i)
+      self.verificaRainha(i) #Soma o risco de todas as rainhas e faz da soma o custo total do tabuleiro
       custo += i.risco
     self.custo = custo
