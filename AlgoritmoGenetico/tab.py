19fe9af90d9be5a9b1765a8159b9ceb1f42b9ea5
Rafael da Silva Lisboa <48603408+RaffeuL@users.noreply.github.com>
2020-12-04 19:56:10 -0300

    Update tab.py

--- AlgoritmoGenetico/tab.py
+++ AlgoritmoGenetico/tab.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from rainha import Rainha
 
-class Tabuleiro(object):
+class Tabuleiro(object): #Re-aproveitamos o tabuleiro feito para o algoritmo de hill climbing e por isso pode ter algumas função que não usamos aqui
   def __init__(self,NumRainhas):
     self.numRainhas = NumRainhas
     self.custo = 0 #Custo total do tabuleiro
@@ -18,47 +18,13 @@ class Tabuleiro(object):
       self.verificaRainha(rainha)
     self.calculaCusto()
     self.estados.append(self.custo)
-
-  def criaTabuleiroPerfeito(self): #Cria um tabuleiro com tudo nuuma linha
-    rainha = Rainha(6,0)
-    self.rainhas.append(rainha)
-    rainha = Rainha(4,1)
-    self.rainhas.append(rainha)
-    rainha = Rainha(2,2)
-    self.rainhas.append(rainha)
-    rainha = Rainha(0,3)
-    self.rainhas.append(rainha)
-    rainha = Rainha(5,4)
-    self.rainhas.append(rainha)
-    rainha = Rainha(7,5)
-    self.rainhas.append(rainha)
-    rainha = Rainha(1,6)
-    self.rainhas.append(rainha)
-    rainha = Rainha(3,7)
-    self.rainhas.append(rainha)
-    self.montaMatriz()
-
-  def criaTabuleiroAleatorio(self): #Cria um tabuleiro full aleatorio
-    for x in range(self.numRainhas):
-      linha = random.randint(0,self.numRainhas-1)
-      coluna = random.randint(0,self.numRainhas-1)
-      rainha = Rainha(linha,coluna)
-      if rainha in self.rainhas:
-        continue
-      self.rainhas.append(rainha)
-    self.montaMatriz()
-  
+ 
   def executa(self): #Faz a movimentação de cada rainha atualizando o custo total do tabuleiro
     for rainha in self.rainhas:
       self.moveRainha(rainha)
       self.calculaCusto()
       print(self.custo)
       self.estados.append(self.custo)#Adiciona o novo custo na lista de estados
-
-    print("="*100)
-    print("CUSTO DA MATRIZ: ",self.custo)
-    
-    print("#"*100)
     return self.custo #Retorna o custo atual
 
   def verificaRainha(self,Rainha):
@@ -86,6 +52,6 @@ class Tabuleiro(object):
     for i in self.rainhas:
       self.verificaRainha(i)
       custo += i.risco
-    self.custo = int(custo/2)
+    self.custo = int(custo/2) #Calcula a quantidade de pares de rainhas que se atacam
 
   
