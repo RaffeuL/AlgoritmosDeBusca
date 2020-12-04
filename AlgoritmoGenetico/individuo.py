a10da9a9553d14871bcf2c36625be022119e4b86
Rafael da Silva Lisboa <48603408+RaffeuL@users.noreply.github.com>
2020-12-04 19:39:54 -0300

    Update individuo.py

--- AlgoritmoGenetico/individuo.py
+++ AlgoritmoGenetico/individuo.py
@@ -1,1 +1,26 @@
+from tab import Tabuleiro
+import math
+class Individuo(object):
+  def __init__(self):
+    self.tabuleiro = Tabuleiro(8)
+    self.tabuleiro.criaTabuleiro()
+    #self.tabuleiro.criaTabuleiroPerfeito()
+    self.cromossomos = []
+    self.geraCromossomo()
+    self.fitness = None
+    self.calculaFitness()
 
+  def __repr__(self):
+    return(str(self.fitness))  
+
+  def geraCromossomo(self):
+    for rainha in self.tabuleiro.rainhas:
+      linha = bin(rainha.linha)[2:]
+      self.cromossomos.append(linha)
+  
+  def calculaFitness(self):
+    self.tabuleiro.calculaCusto()
+    custoMax = int(math.factorial(8)/(2*math.factorial(8-2)))
+    custo = self.tabuleiro.custo
+    fitness = custoMax - custo
+    self.fitness = fitness
